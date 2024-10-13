d631175a8e406dde4392617d22388faf8d6fb1ef
ThiDev17 <thidev.17@gmail.com>
2024-10-13 15:47:36 -0300

    att controller

--- frontend/lib/app/modules/global/sign_in/controller.dart
+++ frontend/lib/app/modules/global/sign_in/controller.dart
@@ -1,10 +1,8 @@
 import 'dart:async';
-
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:get/get.dart';
 import 'package:hospital_management/app/core/routes/routes.dart';
-
 import '../../../core/global_widgets/snackbar.dart';
 import '../../../core/services/auth.dart';
 import 'repository.dart';
@@ -27,12 +25,12 @@ class SignInController extends GetxController {
   // Observáveis para gerenciamento de estado
   final emailError = ''.obs;
   final passwordError = ''.obs;
+  final signUpEmailError = ''.obs;
+  final signUpPasswordError = ''.obs;
   final loading = false.obs;
   final isSignUpFormVisible = false.obs;
 
   bool get getLoading => loading.value;
-
-  get register => null;
 
   @override
   void onClose() {
@@ -71,6 +69,24 @@ class SignInController extends GetxController {
     return isValid;
   }
 
+  Future<bool> signUpFormValidator() async {
+    bool isValid = true;
+    signUpEmailError.value = '';
+    signUpPasswordError.value = '';
+
+    if (signUpEmailController.text.isEmpty || !GetUtils.isEmail(signUpEmailController.text)) {
+      signUpEmailError.value = 'Por favor, insira um email válido';
+      isValid = false;
+    }
+    if (signUpPasswordController.text.isEmpty) {
+      signUpPasswordError.value = 'Por favor, insira uma senha';
+      isValid = false;
+    }
+
+    loading.value = false;
+    return isValid;
+  }
+
   void login() async {
     loading.value = true;
     if (await formValidator()) {
@@ -95,6 +111,33 @@ class SignInController extends GetxController {
     }
   }
 
+  void register() async {
+    loading.value = true;
+    if (await signUpFormValidator()) {
+      try {
+        final response = await signInRepository.registerUser({
+          "fullName": fullNameController.text,
+          "email": signUpEmailController.text,
+          "phone": phoneController.text,
+          "cpf": cpfController.text,
+          "uniqueCode": uniqueCodeController.text,
+          "password": signUpPasswordController.text,
+        });
+
+        if (response.status!) {
+          SnackBarApp.body("Sucesso", "Cadastro realizado com sucesso!");
+          toggleSignUpForm(); // Alterna para a tela de login
+        } else {
+          SnackBarApp.body("Ops!", "Falha ao realizar o cadastro.", icon: FontAwesomeIcons.xmark);
+        }
+      } catch (e) {
+        SnackBarApp.body("Ops!", "Não foi possível realizar o cadastro.", icon: FontAwesomeIcons.xmark);
+      } finally {
+        loading.value = false;
+      }
+    }
+  }
+
   void toggleSignUpForm() {
     isSignUpFormVisible.value = !isSignUpFormVisible.value;
   }
@@ -102,4 +145,8 @@ class SignInController extends GetxController {
   Future<void> logout() async {
     AuthService.to.logoutUser();
   }
+}
+
+extension on SignInRepository {
+  registerUser(Map<String, String> map) {}
 }
